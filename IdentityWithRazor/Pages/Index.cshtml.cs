52b64986a463468c4b69384fe8e5f688549e6ab0
Abu Zaman <md.zaman1994@gmail.com>
2021-11-01 21:45:20 +0600

    Login layout changed

--- IdentityWithRazor/Pages/Index.cshtml.cs
+++ IdentityWithRazor/Pages/Index.cshtml.cs
@@ -5,11 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using IdentityWithRazor.Areas.Identity.Pages.Account;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Services.Services;
+using ServiceStack.Auth;
 
 namespace IdentityWithRazor.Pages
 {
@@ -99,5 +101,90 @@ namespace IdentityWithRazor.Pages
             // If we got this far, something failed, redisplay form
             return Page();
         }
+
+        //public virtual async Task<bool> CheckPasswordAsync(string user, string password)
+        //{
+        //    if (user == null)
+        //    {
+        //        return false;
+        //    }
+
+        //    var result = await VerifyPasswordAsync(passwordStore, user, password);
+        //    if (result == PasswordVerificationResult.SuccessRehashNeeded)
+        //    {
+        //        await UpdatePasswordHash(passwordStore, user, password, validatePassword: false);
+        //        await UpdateUserAsync(user);
+        //    }
+
+        //    var success = result != PasswordVerificationResult.Failed;
+        //    if (!success)
+        //    {
+        //        Logger.LogWarning(0, "Invalid password for user.");
+        //    }
+        //    return success;
+        //}
+
+        //protected virtual async Task<PasswordVerificationResult> VerifyPasswordAsync(IUserPasswordStore<TUser> store, TUser user, string password)
+        //{
+        //    var hash = await store.GetPasswordHashAsync(user, CancellationToken);
+        //    if (hash == null)
+        //    {
+        //        return PasswordVerificationResult.Failed;
+        //    }
+        //    return PasswordHasher.VerifyHashedPassword(user, hash, password);
+        //}
+
+        //public virtual PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
+        //{
+        //    if (hashedPassword == null)
+        //    {
+        //        throw new ArgumentNullException(nameof(hashedPassword));
+        //    }
+        //    if (providedPassword == null)
+        //    {
+        //        throw new ArgumentNullException(nameof(providedPassword));
+        //    }
+
+        //    byte[] decodedHashedPassword = Convert.FromBase64String(hashedPassword);
+
+        //    // read the format marker from the hashed password
+        //    if (decodedHashedPassword.Length == 0)
+        //    {
+        //        return PasswordVerificationResult.Failed;
+        //    }
+        //    switch (decodedHashedPassword[0])
+        //    {
+        //        case 0x00:
+        //            if (VerifyHashedPasswordV2(decodedHashedPassword, providedPassword))
+        //            {
+        //                // This is an old password hash format - the caller needs to rehash if we're not running in an older compat mode.
+        //                return (_compatibilityMode == PasswordHasherCompatibilityMode.IdentityV3)
+        //                    ? PasswordVerificationResult.SuccessRehashNeeded
+        //                    : PasswordVerificationResult.Success;
+        //            }
+        //            else
+        //            {
+        //                return PasswordVerificationResult.Failed;
+        //            }
+
+        //        case 0x01:
+        //            int embeddedIterCount;
+        //            if (VerifyHashedPasswordV3(decodedHashedPassword, providedPassword, out embeddedIterCount))
+        //            {
+        //                // If this hasher was configured with a higher iteration count, change the entry now.
+        //                return (embeddedIterCount < _iterCount)
+        //                    ? PasswordVerificationResult.SuccessRehashNeeded
+        //                    : PasswordVerificationResult.Success;
+        //            }
+        //            else
+        //            {
+        //                return PasswordVerificationResult.Failed;
+        //            }
+
+        //        default:
+        //            return PasswordVerificationResult.Failed; // unknown format marker
+        //    }
+        //}
+
     }
 }
--- Test/ScaffoldingReadMe.txt
+++
@@ -1,6 +0,0 @@
-﻿Support for ASP.NET Core Identity was added to your project.
-
-For setup and configuration information, see https://go.microsoft.com/fwlink/?linkid=2116645.
-
-//DBContext update command
-Scaffold-DbContext "server=RTL-DEV-11;persistsecurityinfo=True;database=Identity" Microsoft.EntityFrameworkCore.SqlServer -OutputDir DAL/Models -f
